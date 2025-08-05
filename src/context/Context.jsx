927f90f006ce3c60f6adb9ae25d1e2f4063202cf
Priyansh Shukla <priyanshshukla930@gmail.com>
2025-08-06 00:37:12 +0530

    refactor

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,17 +1,17 @@
 import { createContext, useState } from "react";
 
 import run from "../config/gemini";
-export const Context =createContext();
+export const Context = createContext();
 
 
-const ContextProvider = (props)=>{
+const ContextProvider = (props) => {
 
-    const[input,setInput]= useState("");
-    const[recentPrompt,setRecentPrompt]= useState("");
-    const[prevPrompts,setPrevPrompts]= useState([]);
-    const[showResult,setShowResult]= useState(false);
-    const[loading,setloading] = useState(false);
-    const[resultData,setResultData]= useState("");
+    const [input, setInput] = useState("");
+    const [recentPrompt, setRecentPrompt] = useState("");
+    const [prevPrompts, setPrevPrompts] = useState([]);
+    const [showResult, setShowResult] = useState(false);
+    const [loading, setloading] = useState(false);
+    const [resultData, setResultData] = useState("");
 
     // const delayPara = () =>{
     //    setTimeout(function (){
@@ -19,43 +19,39 @@ const ContextProvider = (props)=>{
     //    },75*index)
     // }
 
-const onSent = async(prompt) => {
+    const onSent = async (prompt) => {
 
-    setResultData("")
-    setloading(true)
-    setShowResult(true)
-    let response;
-    if(prompt!==undefined)
-    {
-       response=await run(prompt);
-       setRecentPrompt(prompt)
-    }
-    else
-    {
-        setPrevPrompts(prev=>[...prev,input])
+        setResultData("")
+        setloading(true)
+        setShowResult(true)
+        if (prompt !== undefined) {
+
+            setRecentPrompt(prompt)
+        }
+        else {
+            setPrevPrompts(prev => [...prev, input])
+            setRecentPrompt(input)
+
+        }
         setRecentPrompt(input)
-        response = await run(input)
-    }
-    setRecentPrompt(input)
-    setPrevPrompts(prev=>[...prev,input])
-    const oph= await run(input)
-    let responseArray = oph.split("**");
-    
-    let newResponse;
-    for(let i=0;i<responseArray.length;i++)
-    {
-        if(i==0||i%2!==1){
-            newResponse+=responseArray[i];
+        setPrevPrompts(prev => [...prev, input])
+        const oph = await run(input)
+        let responseArray = oph.split("**");
+
+        let newResponse;
+        for (let i = 0; i < responseArray.length; i++) {
+            if (i == 0 || i % 2 !== 1) {
+                newResponse += responseArray[i];
+            }
+            else {
+                newResponse += "<b>" + responseArray[i] + "</b>";
+            }
         }
-        else{
-            newResponse+="<b>"+responseArray[i]+"</b>";
-        }
+        let newResponse2 = newResponse.split("*").join("</br>")
+        setResultData(newResponse2)
+        setloading(false)
+        setInput("")
     }
-    let newResponse2 = newResponse.split("*").join("</br>")
-    setResultData(newResponse2)
-    setloading(false)
-    setInput("")
-}
 
     const contextValue = {
         prevPrompts,
@@ -69,9 +65,9 @@ const onSent = async(prompt) => {
         input,
         setInput
 
-   } 
-    return(
-        <Context.Provider value ={contextValue}>
+    }
+    return (
+        <Context.Provider value={contextValue}>
             {props.children}
         </Context.Provider>
     )
