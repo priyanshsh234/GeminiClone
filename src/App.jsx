f1f16acf508ac5197e7f3468cc882f0e40aec1f2
Priyansh Shukla <priyanshshukla930@gmail.com>
2025-11-23 17:04:35 +0530

    final comit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Sidebar from './components/Sidebar/Sidebar'
-import Main from './components/Main/Main'
+import Main from './components/main/Main'
 
 const App = () => {
   return (
